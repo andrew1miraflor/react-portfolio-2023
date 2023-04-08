3ca0dee74f6ddd0aa6e9aa8c8ce15c718b0a4ded
Andrew Miraflor <andrewjmiraflor@gmail.com>
2023-04-07 22:09:24 -0500

    4-7 Video stopped at 33:33 MVP for Header Component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
 import css from './Header.module.scss'
+import {BiPhoneCall} from 'react-icons/bi'
+
 const Header = () => {
     return (
-        <div>Header</div>
+        <div className={`paddings ${css.wrapper}`}>
+
+            <div className={`flexCenter ${css.container}`}>
+                <div className={css.name}>
+                    Andrew
+                </div>
+
+                <ul className={`flexCenter ${css.menu}`}>
+                    <li><a href="">Services</a></li>
+                    <li><a href="">Experience</a></li>
+                    <li><a href="">Portfolio</a></li>
+                    <li><a href="">Testimonials</a></li>
+                    <li>
+                    <p>+123 456 789</p>
+                    <BiPhoneCall size={"40px"}/>
+
+                    </li>
+                </ul>
+
+            </div>
+        </div>
     )
 }
 export default Header
