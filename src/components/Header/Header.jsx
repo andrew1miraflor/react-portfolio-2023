f141161682736734c1f5e31d3e21718729503bf3
Andrew Miraflor <andrewjmiraflor@gmail.com>
2023-04-22 19:49:42 -0500

    Header animations stopped at 50:18

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,30 +1,49 @@
-import React from 'react'
+import React, { useState } from "react"
 import css from './Header.module.scss'
-import {BiPhoneCall} from 'react-icons/bi'
-
+import { BiPhoneCall, BiMenuAltRight } from 'react-icons/bi'
+import { motion } from 'framer-motion'
+import { getMenuStyles, headerVariants } from "../../utils/motion"
+import useHeaderShadow from "../../hooks/useHeaderShadow";
 const Header = () => {
+
+    const [menuOpened, setMenuOpened] = useState(false);
+    const headerShadow = useHeaderShadow()
     return (
-        <div className={`paddings ${css.wrapper}`}>
+        <motion.div
+            initial="hidden"
+            whileInView="show"
+            variants={headerVariants}
+            viewport={{ once: false, amount: 0.25 }}
+            className={`paddings ${css.wrapper}`
+            }
+            style={{boxShadow: headerShadow}}
+            >
+            <div className={`flexCenter innerWidth ${css.container}`}>
+                <div className={css.name}> Andrew </div>
 
-            <div className={`flexCenter ${css.container}`}>
-                <div className={css.name}>
-                    Andrew
-                </div>
-
-                <ul className={`flexCenter ${css.menu}`}>
+                <ul
+                style={getMenuStyles(menuOpened)}
+                className={`flexCenter ${css.menu}`}>
                     <li><a href="">Services</a></li>
                     <li><a href="">Experience</a></li>
                     <li><a href="">Portfolio</a></li>
                     <li><a href="">Testimonials</a></li>
-                    <li>
-                    <p>+123 456 789</p>
-                    <BiPhoneCall size={"40px"}/>
-
+                    <li className={`flexCenter ${css.phone}`}>
+                        <p>+1 892 892 8392</p>
+                        <BiPhoneCall size={"40px"} />
                     </li>
                 </ul>
+    {/* only for medium and small screens */}
 
+                <div className={css.menuIcon}
+                onClick={()=>setMenuOpened((prev)=>!prev)}
+                >
+                    <BiMenuAltRight size={30} />
+                </div>
             </div>
-        </div>
-    )
-}
+
+        </motion.div>
+        )
+    }
+
 export default Header
